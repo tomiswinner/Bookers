f21c26ed5a42ddd2e75078bd008eefbce53946af
tomiswinner <mouseofsack911@gmail.com>
2021-11-09 09:30:50 +0000

    changed routes for resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-# resources :books
+  resources :books
   root :to => "books#top"
-  get "books" => "books#index",as: "books"
-  get "books/:id" => "books#show",as: "book"
-  get "books/:id/edit" => "books#edit",as:"edit"
+
+  # get "books" => "books#index",as: "books"
+  # get "books/:id" => "books#show",as: "book"
+  # get "books/:id/edit" => "books#edit",as:"edit"
 
 
-  patch "books/:id" => "books#update"
-  post "books" => "books#create"
+  # patch "books/:id" => "books#update"
+  # post "books" => "books#create"
   delete "books/:id" => "books#destroy",as:"destroy"
 end
