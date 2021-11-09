1e368916495f9c03abeb958017f536509f69ff48
tomiswinner <mouseofsack911@gmail.com>
2021-11-09 09:40:52 +0000

    succedd in Rspec test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,7 +33,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
       render :index
     end
