4b7a95d402bff7a5a3ff299198db2b95a9a45ded
EC2 Default User <ec2-user@ip-172-31-2-86.ec2.internal>
2021-11-09 02:43:07 +0000

    implemented error_mes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,6 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
-
   # 詳細ページ
   def show
     @book = Book.find(params[:id])
@@ -23,13 +22,19 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(book.id)
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      render :index
+    end
   end
 
   def destroy
